510c829650eccbb7f8e925c5583a34884ae1bf68
Diogo Júnior <diogo.junior@fraunhofer.pt>
2013-11-01 11:12:17 +0000

    Finish static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 SampleApp::Application.routes.draw do
   get "static_pages/home"
   get "static_pages/help"
+  get "static_pages/about"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -27,7 +28,7 @@ SampleApp::Application.routes.draw do
   #       get 'sold'
   #     end
   #   end
-
+	root :to => "static_pages#home"
   # Example resource route with sub-resources:
   #   resources :products do
   #     resources :comments, :sales
