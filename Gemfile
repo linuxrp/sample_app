dd4c701a6be848717f24292d463194f338b69312
Diogo Júnior <diogo.junior@fraunhofer.pt>
2013-11-01 12:33:58 +0000

    Finish layout and routes

--- Gemfile
+++ Gemfile
@@ -3,6 +3,8 @@ ruby '2.0.0'
 #ruby-gemset=railstutorial_rails_4_0
 
 gem 'rails', '4.0.0'
+gem 'bootstrap-sass', '2.3.2.0'
+gem 'pg', '0.17.0'
 
 group :development, :test do
   gem 'sqlite3', '1.3.8'
@@ -26,6 +28,5 @@ group :doc do
 end
 
 group :production do
-  gem 'pg', '0.15.1'
   gem 'rails_12factor', '0.0.2'
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 SampleApp::Application.routes.draw do
-  get "static_pages/home"
-  get "static_pages/help"
-  get "static_pages/about"
-  # The priority is based upon order of creation: first created -> highest priority.
+  root :to => "static_pages#home"
+  #match '/', to: 'static_pages#home', via: 'get'
+  match '/signup',  to: 'users#new',            via: 'get'
+  match '/help',    to: 'static_pages#help',    via: 'get'
+  match '/about',   to: 'static_pages#about',   via: 'get'
+  match '/contact', to: 'static_pages#contact', via: 'get'
+  
+  
+# The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
@@ -28,7 +33,7 @@ SampleApp::Application.routes.draw do
   #       get 'sold'
   #     end
   #   end
-	root :to => "static_pages#home"
+
   # Example resource route with sub-resources:
   #   resources :products do
   #     resources :comments, :sales
